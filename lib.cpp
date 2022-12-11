44242c77433b04574ffc7213cc2540670db83284
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-11 22:52:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,8 +1,8 @@
 #include "lib.h"
-float max(float num[],int n){
+float max(float num[],int d){
     float a = num[0];
 
-    for(int i=1;i<n;i++){
+    for(int i=1;i<d;i++){
      if(num[i] > a){
          a = mat[i];
         }
