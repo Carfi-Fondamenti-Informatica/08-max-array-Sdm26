13e8458ad1aefc9bb9a70e69643399e4ff6ede8d
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-11 22:40:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "lib.h"
+float max( const float  num[], int d){
+    float c = num[0];
+    for (int i=1; i<d-1;i++){
+        float s=num[i];
+        float b=num[i+1];
+        if(s>b){
+            if(c<s){
+                c=s;
+            }
+        }
+    }
+    return c;
+}
