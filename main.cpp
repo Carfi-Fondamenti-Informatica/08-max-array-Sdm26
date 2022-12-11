89d30c7ae1b0f5fa335404114059f3ca33139fb5
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-11 22:39:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#incude "lib.h"
+using namespace std;
 int main(){
+     int d;
+    cin>>d;
+    float num[d];
+for(int i=0;i<d;i++){
+    cin>>num[i];
+}
+float f= max(num,d);
+cout<<f;
   return 0;
 }
